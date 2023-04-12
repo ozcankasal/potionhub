2c119b6748ab6beee843da78236dc61ab4a52ce2
Özcan Kasal <ozcankasal@gmail.com>
2023-04-12 19:36:00 +0300

    master account changed

--- main.go
+++ main.go
@@ -16,43 +16,52 @@ import (
 func main() {
 	r := gin.Default()
 
-	// Register custom template functions
+	// Conjure custom template functions for our magical templates
 	funcMap := template.FuncMap{
+		// Reveal the current year to the mystical beings of PotionHub
 		"year": func() int {
 			return time.Now().Year()
 		},
+		// Transform potion prices into dazzling dollar format
 		"formatAsDollars": utils.FormatAsDollars,
-		"join":            strings.Join,
+		// Join strings as if weaving a powerful incantation
+		"join": strings.Join,
 	}
 
-	// Load templates with custom functions
+	// Enchant the Gin router with custom functions and summon the templates
 	r.SetFuncMap(funcMap)
 	r.LoadHTMLGlob("templates/*.html")
 
-	// Set up session middleware
+	// Set up a magical session store to remember the identity of visitors
 	store := cookie.NewStore([]byte("secret-key"))
 	r.Use(sessions.Sessions("mysession", store))
 
+	// Prepare the LoggedIn middleware to guard our enchanted routes
 	loggedInMiddleware := middleware.LoggedIn()
 
+	// Unveil the catalog of potions, protected by the LoggedIn middleware
 	catalogController := controllers.NewCatalogController()
 	r.GET("/catalog", loggedInMiddleware, catalogController.GetCatalog)
 
+	// Reveal the entrance to the authentication chamber
 	authController := controllers.NewAuthController()
 	r.GET("/login", authController.GetLogin)
 
-	// Replace "fixedUsername" and "fixedPassword" with your desired values
-	authMiddleware := middleware.AuthRequired("admin", "admin")
+	// Secure the authentication chamber with a mystical password lock
+	authMiddleware := middleware.AuthRequired("SpellBinder", "ElixirOfEnigma")
 	r.POST("/login", authMiddleware, authController.PostLogin)
 
+	// Greet newcomers to PotionHub with a warm and welcoming spell
 	welcomeController := controllers.NewWelcomeController()
 	r.GET("/welcome", welcomeController.GetWelcome)
 
+	// Display the magical home of PotionHub, accessible only to worthy adventurers
 	homeController := controllers.NewHomeController()
 	r.GET("/", loggedInMiddleware, homeController.GetHome)
 
-	// Logout route
+	// Create a secret passage for adventurers to leave the mystical realm
 	r.GET("/logout", authController.Logout)
 
+	// Summon the Gin framework to listen for and serve the magical beings
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
